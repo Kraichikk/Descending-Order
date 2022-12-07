60de2c260ebb946a23c009adde2cdb5cfdaa780e
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2022-12-07 13:17:00 +0200

    Add project files.

---
+++ Descending Order/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Descending_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 123456789;
+            Console.WriteLine(DescendingOrder(num));
+        }
+        //MySolution
+        public static int DescendingOrder(int num)
+        {
+            string strNum = num.ToString();
+            var list = strNum.ToList();
+
+            list.Sort();
+            list.Reverse();
+
+            strNum = String.Join("", list);
+
+            return Convert.ToInt32(strNum);
+        }
+        //more clever solution
+        public static int DescendingOrder1(int num)
+        {
+            return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
+        }
+    }
+}
